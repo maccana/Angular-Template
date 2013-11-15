8eeff9becd99c0f512ee93ab4696858dce5a2691
maccana <mikeymccann@gmail.com>
2013-11-15 01:46:35 +0000

    Added addRemoveGreetiing controller

--- app/app.js
+++ app/app.js
@@ -6,13 +6,23 @@ angular.module('hello', [])
 		})
 
 		var WorldCtrl = function ($scope) { 
-		$scope.population = 7000; 
-		$scope.countries = [
+			$scope.population = 7000; 
+			$scope.countries = [
 							{name: 'France', population: 63.1}, 
 							{name: 'United Kingdom', population: 61.8},
 						]; 	
 
-		$scope.worldsPercentage = function (countryPopulation) { 
-			return Math.round((countryPopulation / $scope.population)* 100) / 100;
-		}	
-	}
+			$scope.worldsPercentage = function (countryPopulation) { 
+				return Math.round((countryPopulation / $scope.population)* 100) / 100;
+			}	
+		}
+		var addRemoveGreetingCtrl = function ($scope) {
+			$scope.name = "";
+
+			$scope.$watch("name", function(newValue, oldValue){
+				if ($scope.name.length > 0) {
+					$scope.text = "Greetings " + $scope.name;
+				} else 
+					$scope.text = "";
+			});
+		} 
