113445106fbb89bb17835275e73a4166be28d394
maccana <mikeymccann@gmail.com>
2014-07-08 21:18:38 -0400

    Updates Todo app:Implemented addToDo function,item check boxes,json file clean-up

--- ToDo_app/js/app.js
+++ ToDo_app/js/app.js
@@ -2,7 +2,15 @@ var app = angular.module('app', []);
 
 app.controller('Ctrl1', function Ctrl1($scope, $http) {
 	$http.get('js/items.json').success(function(data){
-			$scope.items = data;
+			$scope.todos = data;
+			console.log($scope.todos);
 			$scope.itemOrder = 'name';
-		});
+	});
+
+	$scope.addTodo = function () {
+	 		console.log("hello");
+	    	$scope.todos.push({name:$scope.formTodoText, done:false});
+	    	$scope.formTodoText = '';
+	 };
+
 });
