a98439c291920f7d378f4172f34f9f0eddc5e385
maccana <mikeymccann@gmail.com>
2014-07-12 15:48:22 -0400

    Added todo route and ctrl

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ angular.module('templates', [
 			'ui.bootstrap',
 			'MyComponents'
 		]) 
+/* Configuration of ui-router to inject views into ui-view element in index.html */
 		.config(function ($stateProvider, $urlRouterProvider){
 
 			$stateProvider
@@ -26,7 +27,7 @@ angular.module('templates', [
 
 				})
 				.state('list.item', {
-					url : '/:item',
+					url : '/:item', /* NOTE: ng knows to append item param to list url */
 					templateUrl: 'partials/list.item.html',
 					controller: function ($scope, $stateParams) {
 						$scope.item = $stateParams.item;
@@ -58,7 +59,10 @@ angular.module('templates', [
 
 		})
 
+/* CONTROLLERS */
+/* -------------------------------------------------------------------------- List ctrl example ---- */
 		.controller('listCtrl', function($scope){
+			/* itemList is bound to the list view via scope */
 			$scope.itemList = [
 				{name: "French"},
 				{name: "Guitar"},
@@ -68,41 +72,62 @@ angular.module('templates', [
 
 			];
 		})
-				/* Text area with limit to be implemented */
+
+
+/* ------------------------------------------------------------------------- Todos ctrl example ---- */
+		function TodoCtrl($scope,$http) {
+		 	$http.get('js/items.json').success(function(data){
+			$scope.todos = data;
+			})
+
+			$scope.addTodo = function() {
+		    $scope.todos.push({text:$scope.todoText, done:false});
+		    $scope.todoText = '';
+		  	};
+		 
+		 
+	  	$scope.remaining = function() {
+		    var count = 0;
+		    angular.forEach($scope.todos, function(todo) {
+		      count += todo.done ? 0 : 1;
+		    });
+		    return count;
+	  	};
+	 
+	  	$scope.archive = function() {
+		    var oldTodos = $scope.todos;
+		    $scope.todos = [];
+		    angular.forEach(oldTodos, function(todo) {
+		      if (!todo.done) $scope.todos.push(todo);
+		    });
+		};
+	}
+
+/* -------------------------------------------------------------- Limited text area ctrl example ---- */		
+		/* Text area with limit to be implemented */
 		.controller('TextAreaWithLimitCtrl', function($scope){
 			$scope.remaining = function () {
 			return MAX_LEN - $scope.message.length;
 			};
 		})
 
-		/* Todo controller example */
-		function TodoCtrl($scope) {
-		  $scope.todos = [
-		    {text:'learn angular', done:true},
-		    {text:'build an angular app', done:false}];
-		 
-		  $scope.addTodo = function() {
-		    $scope.todos.push({text:$scope.todoText, done:false});
-		    $scope.todoText = '';
-		  };
-		 
-		  $scope.remaining = function() {
-		    var count = 0;
-		    angular.forEach($scope.todos, function(todo) {
-		      count += todo.done ? 0 : 1;
-		    });
-		    return count;
-		  };
-		 
-		  $scope.archive = function() {
-		    var oldTodos = $scope.todos;
-		    $scope.todos = [];
-		    angular.forEach(oldTodos, function(todo) {
-		      if (!todo.done) $scope.todos.push(todo);
-		    });
-		  };
-		}
+/* -------------------------------------------------------------------- Playlist ctrl example ------- */
+		/* Prototype for dynamic interactive audio playlist 
+		- To be implemented */	
+		var playlistCtrl = function($scope,$filter) {
+			$scope.selectedAlbumSongs = [
+				{'name': 'song1', 'url': 'http://test/song1.mp3' },
+				{'name': 'song2', 'url': 'http://test/song2.mp3' },
+				{'name': 'song3', 'url': 'http://test/song3.mp3' }
 
+			];
+			$scope.selectedSongs = function () {
+        		$scope.playList = $filter('filter')($scope.selectedAlbumSongs, {checked: true});
+			}
+		}		
+
+/* ------------------------------------------------------------------- Population ctrl example ------ */
+		/* To be implemented */
 		var WorldCtrl = function ($scope) { 
 			$scope.population = 7000; 
 			$scope.countries = [
@@ -116,19 +141,8 @@ angular.module('templates', [
 			}	
 		}
 
-		/* Prototype for dynamic interactive audio playlist */		
-		var playlistCtrl = function($scope,$filter) {
-			$scope.selectedAlbumSongs = [
-				{'name': 'song1', 'url': 'http://test/song1.mp3' },
-				{'name': 'song2', 'url': 'http://test/song2.mp3' },
-				{'name': 'song3', 'url': 'http://test/song3.mp3' }
-
-			];
-			$scope.selectedSongs = function () {
-        		$scope.playList = $filter('filter')($scope.selectedAlbumSongs, {checked: true});
-			}
-		}
-
+/* -------------------------------------------------------------------- Greeting ctrl example ------- */
+		/* To be implemented */
 		var addRemoveGreetingCtrl = function ($scope) {
 			$scope.name = "";
 			$scope.$watch("name", function(newValue, oldValue){
@@ -139,7 +153,9 @@ angular.module('templates', [
 			});
 		} 
 
-		/* Alternative syntax for controller function */
+/* -------------------------------------------------------------------- Voting ctrl example --------- */
+		/* Alternative syntax for controller function 
+		- To be implemented */
 		function incrementDecrementValue($scope){ 
 			$scope.value = 1;
 				$scope.incrementValue = function(value) { 
