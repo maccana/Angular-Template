8d90a7ea621b6b849a2c47b48289a0eeb47b3357
maccana <mikeymccann@gmail.com>
2014-07-24 00:24:36 -0400

    Added calculator ctrl,clean up

--- app/app.js
+++ app/app.js
@@ -1,247 +1,273 @@
 'use strict';
 
 /* App Module - Place app dependencies inside array */
-var app = angular.module('templates', [
-			'ui.router',
-			'ui.bootstrap',
-			'MyComponents',
-			'firebase'
-		]) 
-/* Configuration of ui-router to inject views into ui-view element in index.html */
-		.config(function ($stateProvider, $urlRouterProvider){
+var templates = angular.module('templates', [
+		'ui.router',
+		'ui.bootstrap',
+		'MyComponents',
+		'firebase'
+	]) 
+	/* Configuration of ui-router to inject views into ui-view element in index.html */
+	.config(function ($stateProvider, $urlRouterProvider){
+		$stateProvider
+			.state('/', {
+				url : '/',
+				templateUrl : 'partials/login.html',
+				controller : 'loginCtrl'
+			})
+			.state('home', {
+				url : '/home',
+				templateUrl: 'partials/home.html'
 
-			$stateProvider
-				.state('/', {
-					url : '/',
-					templateUrl : 'partials/login.html',
-					controller : 'loginCtrl'
-				})
-				.state('home', {
-					url : '/home',
-					templateUrl: 'partials/home.html'
+			})
+			.state('contact', {
+				url : '/contact',
+				templateUrl: 'partials/contact.html'
 
-				})
-				.state('contact', {
-					url : '/contact',
-					templateUrl: 'partials/contact.html'
+			})
+			.state('list', {
+				url : '/list',
+				templateUrl: 'partials/list.html',
+				controller: 'listCtrl'
 
-				})
-				.state('list', {
-					url : '/list',
-					templateUrl: 'partials/list.html',
-					controller: 'listCtrl'
-
-				})
-				.state('list.item', {
-					url : '/:item', /* NOTE: ng knows to append item param to list url */
-					templateUrl: 'partials/list.item.html',
-					controller: function ($scope, $stateParams) {
-						$scope.item = $stateParams.item;
-					}
-				})
-				.state('toggle', {
-					url : '/toggle',
-					templateUrl: 'partials/toggle.html',
-					controller:	function toggleImage($scope){
-						$scope.visible = true; 
-						$scope.toggle = function() {
-							var btn = document.getElementById('toggle-btn');
-							if(btn.innerHTML == 'Show Image') {
-								btn.innerHTML = 'Hide Image';
-							} 
-							else 
-							btn.innerHTML = 'Show Image';
-							$scope.visible = !$scope.visible;
-						}
-					}
-				})
-				.state('todo', {
-					url: '/todo',
-					templateUrl: 'partials/todo.html',
-					controller: 'TodoCtrl'
-				})
-				.state('storage', {
-					url: '/storage',
-					templateUrl: 'partials/storage.html',
-					controller: 'MaintCtrl'
-				})
-				.state('firebase', {
-					url: '/firebase',
-					templateUrl: 'partials/firebase.html',
-					controller: 'firebaseCtrl'
-				})
-		})
-
-/* CONTROLLERS */
-/* -------------------------------------------------------------------------- List ctrl example ---- */
-		.controller('listCtrl', function($scope){
-			/* itemList is bound to the list view via scope */
-			$scope.itemList = [
-				{name: "French"},
-				{name: "Guitar"},
-				{name: "Jog"},
-				{name: "Wine"},
-				{name: "Angular"},
-
-			];
-		})
-/* -------------------------------------------------------------- Login ctrl example ---------------- */	
-		.controller('loginCtrl', function($scope){ 
-			$scope.login = function(){
-				if($scope.loginForm.$valid) {
-					console.log('sending request....');
+			})
+			.state('list.item', {
+				url : '/:item', /* NOTE: ng knows to append item param to list url */
+				templateUrl: 'partials/list.item.html',
+				controller: function ($scope, $stateParams) {
+					$scope.item = $stateParams.item;
 				}
-			};
+			})
+			.state('toggle', {
+				url : '/toggle',
+				templateUrl: 'partials/toggle.html',
+				controller:	function toggleImage($scope){
+					$scope.visible = true; 
+					$scope.toggle = function() {
+						var btn = document.getElementById('toggle-btn');
+						if(btn.innerHTML == 'Show Image') {
+							btn.innerHTML = 'Hide Image';
+						} 
+						else 
+						btn.innerHTML = 'Show Image';
+						$scope.visible = !$scope.visible;
+					}
+				}
+			})
+			.state('todo', {
+				url: '/todo',
+				templateUrl: 'partials/todo.html',
+				controller: 'TodoCtrl'
+			})
+			.state('storage', {
+				url: '/storage',
+				templateUrl: 'partials/storage.html',
+				controller: 'MaintCtrl'
+			})
+			.state('firebase', {
+				url: '/firebase',
+				templateUrl: 'partials/firebase.html',
+				controller: 'firebaseCtrl'
+			})
+			.state('/calculate', {
+				url : '/calculate',
+				templateUrl : 'partials/calc.html',
+				controller : 'CalculatorCtrl'
+			})	
+	})
 
-		})
-/* -------------------------------------------------------------- Limited text area ctrl example ---- */		
-		/* Text area with limit to be implemented */
-		.controller('TextAreaWithLimitCtrl', function($scope){
-			$scope.remaining = function () {
-			return MAX_LEN - $scope.message.length;
-			};
-		})
+/* ===================================== CONTROLLERS ============================================*/
 
-/* -------------------------------------------------------------- Storage ctrl example -------------- */
+/* ----------------------------------------------------------------------------- Calculator Ctrl */
+
+	.controller('CalculatorCtrl', function($scope, CalculatorService) {
+	 
+	    $scope.doSquare = function() {
+	        $scope.answer = CalculatorService.square($scope.number);
+	    }
+	 
+	    $scope.doCube = function() {
+	        $scope.answer = CalculatorService.cube($scope.number);
+	    }
+	})
+
+/* ----------------------------------------------------------------------------------- List Ctrl */
+	
+	.controller('listCtrl', function($scope){
+		/* itemList is bound to the list view via scope */
+		$scope.itemList = [
+			{name: "French"},
+			{name: "Guitar"},
+			{name: "Jog"},
+			{name: "Wine"},
+			{name: "Angular"},
+
+		];
+	})
+/* --------------------------------------------------------------------------------- Login Ctrl  */	
+
+.controller('loginCtrl', function($scope){ 
+	$scope.login = function(){
+		if($scope.loginForm.$valid) {
+			console.log('sending request....');
+		}
+	};
+
+})
+
+/* -------------------------------------------------------------------------------- Storage Ctrl */
+
+// To be continued
 .controller("MaintCtrl", function($scope, LS) {
-  $scope.greeting = "Local storage demo app";
-  $scope.value = LS.getData();
-  $scope.latestData = function() {
-    return LS.getData();
-  };
-  this.update = function(val) {
-    return LS.setData(val);
-  };
+$scope.greeting = "Local storage demo app";
+$scope.value = LS.getData();
+$scope.latestData = function() {
+return LS.getData();
+};
+this.update = function(val) {
+return LS.setData(val);
+};
 })
 
 .factory("LS", function($window, $rootScope) {
-	
-  angular.element($window).on('storage', function(event) {
-    if (event.key === 'my-storage') {
-      $rootScope.$apply();
-    }
-  });
-  return {
-    setData: function(val) {
-    	console.log("hello2");
-      $window.localStorage && $window.localStorage.setItem('my-storage', val);
-      return this;
-    },
-    getData: function() {
-    	// console.log("hello");
-      return $window.localStorage && $window.localStorage.getItem('my-storage');
-    }
-  };
+
+angular.element($window).on('storage', function(event) {
+if (event.key === 'my-storage') {
+  $rootScope.$apply();
+}
+});
+return {
+setData: function(val) {
+	console.log("hello2");
+  $window.localStorage && $window.localStorage.setItem('my-storage', val);
+  return this;
+},
+getData: function() {
+	// console.log("hello");
+  return $window.localStorage && $window.localStorage.getItem('my-storage');
+}
+};
 });
 
-/* ---------------------------------------------------------------------- Firebase ctrl example ---- */
-		function firebaseCtrl($scope, $firebase) { 
-			var ref = new Firebase("https://sb1m27fx608.firebaseio-demo.com/");
-			$scope.messages = $firebase(ref);
-		}
-/* ------------------------------------------------------------------------- Todos ctrl example ---- */
-
-		function TodoCtrl($scope,$http) {
-		 	$http.get('js/TodoList.json').success(function(data){
-			$scope.todos = data;
-			})
-
-			$scope.addTodo = function() {
-		    	$scope.todos.push({text:$scope.todoText, done:false});
-		    	$scope.todoText = '';
-		  	};
-		 
-		 
-	  		$scope.remaining = function() {
-		    	var count = 0;
-		    	angular.forEach($scope.todos, function(todo) {
-		      		count += todo.done ? 0 : 1;
-		    	});
-		    	return count;
-	  		};
-	  		$scope.removeTodo = function(todo) {
-	  			$scope.todos.splice($scope.todos.indexOf(todo),1);
-	  			console.log(todo.text + " has been removed from list.");
-	  		};
-	 
-	  		$scope.archive = function() {
-		    var oldTodos = $scope.todos;
-		    	$scope.todos = [];
-		    	angular.forEach(oldTodos, function(todo) {
-		      		if (!todo.done) $scope.todos.push(todo);
-		    });
-		};
+/* ------------------------------------------------------------------------------= Firebase Ctrl */
+	
+	/* alternate controller structure */
+	function firebaseCtrl($scope, $firebase) { 
+		var ref = new Firebase("https://sb1m27fx608.firebaseio-demo.com/");
+		$scope.messages = $firebase(ref);
 	}
 
-/* -------------------------------------------------------------------- Playlist ctrl example ------- */
-		/* Prototype for dynamic interactive audio playlist 
-		- To be implemented */	
-		var playlistCtrl = function($scope,$filter) {
-			$scope.selectedAlbumSongs = [
-				{'name': 'song1', 'url': 'http://test/song1.mp3' },
-				{'name': 'song2', 'url': 'http://test/song2.mp3' },
-				{'name': 'song3', 'url': 'http://test/song3.mp3' }
+/* ---------------------------------------------------------------------------------- Todos Ctrl */
+	
+	function TodoCtrl($scope,$http) {
+	 	$http.get('js/TodoList.json').success(function(data){
+		$scope.todos = data;
+		})
+		$scope.addTodo = function() {
+	    	$scope.todos.push({text:$scope.todoText, done:false});
+	    	$scope.todoText = '';
+	  	};
+  		$scope.remaining = function() {
+	    	var count = 0;
+	    	angular.forEach($scope.todos, function(todo) {
+	      		count += todo.done ? 0 : 1;
+	    	});
+	    	return count;
+  		};
+  		$scope.removeTodo = function(todo) { /* passed todo as $index not working */
+  			$scope.todos.splice($scope.todos.indexOf(todo),1);
+  			console.log(todo.text + " has been removed from list.");
+  		};
+ 
+  		$scope.archive = function() {
+	    	var oldTodos = $scope.todos;
+	    	$scope.todos = [];
+	    	angular.forEach(oldTodos, function(todo) {
+	      		if (!todo.done) $scope.todos.push(todo);
+	    });
+	};
+};
 
-			];
-			$scope.selectedSongs = function () {
-        		$scope.playList = $filter('filter')($scope.selectedAlbumSongs, {checked: true});
-			}
-		}		
+/* ---------------------------------------------------------------------- Limited text area Ctrl */		
 
-/* ------------------------------------------------------------------- Population ctrl example ------ */
-		/* To be implemented */
-		var WorldCtrl = function ($scope) { 
-			$scope.population = 7000; 
-			$scope.countries = [
-							{name: 'France', population: 63.1}, 
-							{name: 'United Kingdom', population: 61.8},
-							{name: 'Republic of Ireland', population: 4.8},
-						]; 	
+/* Text area with limit to be implemented */
+function TextAreaWithLimit($scope){
+	var MAX_LEN = 140;
+	$scope.remaining = function () {
+	return MAX_LEN - $scope.message.length;
+	};
+}
 
-			$scope.worldsPercentage = function (countryPopulation) { 
-				return Math.round((countryPopulation / $scope.population)* 100) / 100;
-			}	
-		}
+/* ------------------------------------------------------------------------------- Playlist Ctrl */
 
-/* -------------------------------------------------------------------- Greeting ctrl example ------- */
-		/* To be implemented */
-		var addRemoveGreetingCtrl = function ($scope) {
-			$scope.name = "";
-			$scope.$watch("name", function(newValue, oldValue){
-				if ($scope.name.length > 0) {
-					$scope.text = "Greetings " + $scope.name;
-				} else 
-					$scope.text = "";
-			});
-		} 
+/* To be implemented - Prototype for dynamic interactive audio playlist */	
+var playlistCtrl = function($scope,$filter) {
+	$scope.selectedAlbumSongs = [
+		{'name': 'song1', 'url': 'http://test/song1.mp3' },
+		{'name': 'song2', 'url': 'http://test/song2.mp3' },
+		{'name': 'song3', 'url': 'http://test/song3.mp3' }
 
-/* -------------------------------------------------------------------- Voting ctrl example --------- */
-		/* Alternative syntax for controller function 
-		- To be implemented */
-		function incrementDecrementValue($scope){ 
-			$scope.value = 1;
-				$scope.incrementValue = function(value) { 
-					$scope.value += 1;
-				}
-				$scope.resetValue = function() {
-					$scope.value = 1;
-					//console.log('reset'); //debugging in console
-				}
-				$scope.decrementValue = function(value) {
-					$scope.value -= 1;
+	];
+	$scope.selectedSongs = function () {
+		$scope.playList = $filter('filter')($scope.selectedAlbumSongs, {checked: true});
+	}
+}		
 
-				}
-		}
+/* ----------------------------------------------------------------------------- Population Ctrl */
+
+/* To be implemented - display populations and percentage of world population */
+var WorldCtrl = function ($scope) { 
+	$scope.population = 7000; 
+	$scope.countries = [
+		{name: 'France', population: 63.1}, 
+		{name: 'United Kingdom', population: 61.8},
+		{name: 'Republic of Ireland', population: 4.8},
+	]; 	
+
+	$scope.worldsPercentage = function (countryPopulation) { 
+		return Math.round((countryPopulation / $scope.population)* 100) / 100;
+	}	
+}
+
+/* ------------------------------------------------------------------------------ Greeting Ctrl */
+
+/* To be implemented - dynamically add / remove greeting on page */
+var addRemoveGreetingCtrl = function ($scope) {
+	$scope.name = "";
+	$scope.$watch("name", function(newValue, oldValue){
+		if ($scope.name.length > 0) {
+			$scope.text = "Greetings " + $scope.name;
+		} else 
+			$scope.text = "";
+	});
+} 
+
+/* -------------------------------------------------------------------------------- Voting Ctrl */
+
+/* Alternative syntax for controller function 
+- To be implemented */
+function incrementDecrementValue($scope){ 
+	$scope.value = 1;
+	$scope.incrementValue = function(value) { 
+		$scope.value += 1;
+	}
+	$scope.resetValue = function() {
+		$scope.value = 1;
+		//console.log('reset'); //debugging in console
+	}
+	$scope.decrementValue = function(value) {
+		$scope.value -= 1;
+	}
+}
+
 
 
 
 
 	
-		
 
 
 
 
-		
-		
+	
 	
