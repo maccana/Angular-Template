b61a330eb866abdce6ba381d5ea742cfb5eec3b6
maccana <mikeymccann@gmail.com>
2014-07-24 00:27:19 -0400

    Added calculator services

--- app/services/services.js
+++ app/services/services.js
@@ -1,6 +1,28 @@
 'use strict';
 
-/* Services */
+/* ===================================== SERVICES ===========================================*/
 
-angular.module('templates.services', []),
-	value('version', '0.2');
+/* ----------------------------------------------------------------------- CalculatorService */ 
+
+templates.service('CalculatorService', function(MathService){
+
+  this.square = function(a) { return MathService.multiply(a,a); };
+
+  this.cube = function(a) { return MathService.multiply(a, MathService.multiply(a,a)); };
+})
+
+/* ----------------------------------------------------------------------------- MathService */
+
+templates.service('MathService', function() {
+  
+  this.add = function(a, b) { return a + b };
+   
+  this.subtract = function(a, b) { return a - b };
+   
+  this.multiply = function(a, b) { return a * b };
+   
+  this.divide = function(a, b) { return a / b };
+})
+
+
+
