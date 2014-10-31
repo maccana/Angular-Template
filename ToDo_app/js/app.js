c6da94cf979ab510975e768303229a111cac8823
maccana <mikeymccann@gmail.com>
2014-10-31 01:04:07 -0400

    update todo app.js

--- ToDo_app/js/app.js
+++ ToDo_app/js/app.js
@@ -3,12 +3,12 @@ var app = angular.module('app', []);
 app.controller('Ctrl1', function Ctrl1($scope, $http) {
 	$http.get('js/items.json').success(function(data){
 			$scope.todos = data;
-			console.log($scope.todos);
+			console.log(data);
 			$scope.itemOrder = 'name';
 	});
 
 	$scope.addTodo = function () {
-	 		console.log("hello");
+	 		console.log("Todo added");
 	    	$scope.todos.push({name:$scope.formTodoText, done:false});
 	    	$scope.formTodoText = '';
 	 };
