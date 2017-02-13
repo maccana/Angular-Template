ff53e76eac7dede1d9be4adbcc0685698f1a1d95
maccana <mikeymccann@gmail.com>
2017-02-13 18:03:16 -0500

    Refactor firebase module and styles

--- app/controllers/firebase.js
+++ app/controllers/firebase.js
@@ -2,40 +2,41 @@
 /* Firebase Ctrl */
 
 templates.controller('FirebaseCtrl', function($scope, $firebase) {
- $scope.pageTitle = 'Firebase Example';
+  // Title for page saved to $scope var
+  $scope.pageTitle = 'Firebase Example';
 
- // var ref = new Firebase("https://sb1m27fx608.firebaseio-demo.com/");
- // var ref = new Firebase("https://employee-list.firebaseio.com/Employees");
- //
- // $scope.messages = $firebase(ref);
+  // var ref = new Firebase("https://sb1m27fx608.firebaseio-demo.com/");
+  // var ref = new Firebase("https://employee-list.firebaseio.com/Employees");
 
- $scope.employeeName = "";
-	$scope.employeeAge = 0;
-	$scope.employees = {};
-	// var ref = new Firebase("https://kf1b63m1vnc.firebaseio-demo.com/");
-	$scope.myData = new Firebase("https://employee-list.firebaseio.com/Employees");
-	$scope.employeeData = $firebase($scope.myData);
+  $scope.employeeName = '';
+  $scope.employeeRole = '';
+  $scope.employees = {};
+  // var ref = new Firebase("https://kf1b63m1vnc.firebaseio-demo.com/");
+  $scope.employeesRef = new Firebase("https://employee-list.firebaseio.com/Employees");
+  $scope.employeeData = $firebase($scope.employeesRef);
 
-	//var employeeList = JSON.stringify($scope.employeeData);
+  //var employeeList = JSON.stringify($scope.employeeData);
+  $scope.saveEmployee = function() {
+  	$scope.employeesRef.push({
+      employeeName:$scope.employeeName,
+  		employeeRole:$scope.employeeRole
+  	});
+  	//alert("You have saved: " + $scope.employeeName + " Aged: " + $scope.employeeAge);
+  	$scope.employeeName = '';
+  	$scope.employeeRole = '';
+  }
 
-	$scope.saveEmployee = function() {
-		$scope.myData.push({employeeName:$scope.employeeName,
-							employeeAge:$scope.employeeAge
-		});
-		//alert("You have saved: " + $scope.employeeName + " Aged: " + $scope.employeeAge);
-		$scope.employeeName = "";
-		$scope.employeeAge = 0;
-	}
+  $scope.deleteEmployee = function(key,name) {
+    console.log('Obj ref key in firebase: ', key);
+  	// get ref to db collection
+  	var employeeRef = new Firebase("https://employee-list.firebaseio.com/Employees/" + key);
+    employeeRef.remove();
+  }
 
-	$scope.deleteEmployee = function(key,name) {
-		// get ref to db collection
-		var employeeRef = new Firebase("https://employee-list.firebaseio.com/Employees");
-
-	}
-
-	$scope.myData.on('value', function(snapshot) {
-		$scope.employees = snapshot.val();
-		$scope.$apply(); // updates employee list on page refresh
-	});
+  // Keeping employees table updated in real-time
+  $scope.employeesRef.on('value', function(snapshot) {
+  	$scope.employees = snapshot.val();
+  	$scope.$apply(); // updates employee list on page refresh
+  });
 
 })
