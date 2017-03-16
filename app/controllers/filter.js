11cdee6269b59ad818554aa8a275eb74fedfc6ed
maccana <mikeymccann@gmail.com>
2017-03-15 23:24:54 -0400

    Added controller for filter examples

---
+++ app/controllers/filter.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* Filter Ctrl */
+
+templates.controller('FilterCtrl', function($scope) {
+  // $scope.msg = 'filter ctrl connected'; // debugging
+
+  $scope.friends = [
+    { name: "Peter",   age: 20 },
+    { name: "Pablo",   age: 55 },
+    { name: "Linda",   age: 20 },
+    { name: "Marta",   age: 37 },
+    { name: "Othello", age: 20 },
+    { name: "Markus",  age: 32 }
+  ];
+})
