cc627df3714a383b3ae693590775a3cc8504ab31
maccana <mikeymccann@gmail.com>
2017-03-19 18:44:44 -0400

    Refactored HttpCtrl with IIFE

--- app/controllers/http.js
+++ app/controllers/http.js
@@ -1,12 +1,12 @@
-'use strict';
-
 /* HTTP Ctrl */
-
-templates.controller('HttpCtrl', function($scope, $http) {
-  // $scope.msg = 'http ctrl connected'; debugging
-  $http.get("partials/httpMsg.html")
-    .then(function(response) {
-        $scope.welcome = response.data;
-  });
-
-})
+(function() {
+  'use strict';
+  function HttpController($scope, $http) {
+      $http.get("partials/httpMsg.html")
+        .then(function(response) {
+            $scope.welcome = response.data;
+      });
+  }
+  angular.module('templates')
+  .controller('HttpCtrl', HttpController);
+})();
