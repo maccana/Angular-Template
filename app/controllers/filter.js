3f902c8fddf8dea93e8e3705e8fcfcd4c707ec10
maccana <mikeymccann@gmail.com>
2017-03-21 23:06:44 -0400

    Modified filter.js

--- app/controllers/filter.js
+++ app/controllers/filter.js
@@ -1,16 +1,19 @@
-'use strict';
+(function() {
 
-/* Filter Ctrl */
+  "use strict";
 
-templates.controller('FilterCtrl', function($scope) {
-  // $scope.msg = 'filter ctrl connected'; // debugging
+  /* Filter Ctrl */
+  angular.module('templates')
+    .controller('FilterCtrl', function($scope) {
+      // $scope.msg = 'filter ctrl connected'; // debugging
 
-  $scope.friends = [
-    { name: "Peter",   age: 20 },
-    { name: "Pablo",   age: 55 },
-    { name: "Linda",   age: 20 },
-    { name: "Marta",   age: 37 },
-    { name: "Othello", age: 20 },
-    { name: "Markus",  age: 32 }
-  ];
-})
+      $scope.friends = [
+        { name: "Peter",   age: 20 },
+        { name: "Pablo",   age: 55 },
+        { name: "Linda",   age: 20 },
+        { name: "Marta",   age: 37 },
+        { name: "Othello", age: 20 },
+        { name: "Markus",  age: 32 }
+      ];
+    })
+})();
