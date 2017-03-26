e74c28d19b8eb3ac20cceb971594c11cc4011948
maccana <mikeymccann@gmail.com>
2017-03-26 14:22:50 -0400

    Update timeout controller

--- app/controllers/timeout.js
+++ app/controllers/timeout.js
@@ -1,10 +1,13 @@
-'use strict';
-
 /* Timeout Ctrl */
+(function() {
 
-templates.controller('TimeoutCtrl', function($scope, $timeout) {
-  $scope.message = "Wait for the message to turn to a greetng in 3 seconds!";
-    $timeout(function () {
-        $scope.message = "Hey, thanks for hanging around to see this awesome $timeout service!";
-    }, 3000);
-})
+  'use strict';
+
+  function TimeoutController($scope, $timeout) {
+    $scope.message = "Wait for the message to turn to a greetng in 3 seconds!";
+      $timeout(function () {
+          $scope.message = "Hey, thanks for hanging around to see this awesome $timeout service!";
+      }, 3000);
+  }
+  angular.module('templates').controller('TimeoutCtrl', TimeoutController);
+})();
