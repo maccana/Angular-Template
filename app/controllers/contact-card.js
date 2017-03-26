86463716658edea156871ac119bb9c2ab0ab3726
maccana <mikeymccann@gmail.com>
2017-03-26 14:22:22 -0400

    Update contact-card controller

--- app/controllers/contact-card.js
+++ app/controllers/contact-card.js
@@ -1,33 +1,37 @@
-'use strict';
+(function() {
 
-/*  Dir1 Ctrl - contact-card */
+  "use strict";
 
-templates.controller('ContactCardCtrl', function($scope) {
- $scope.pageTitle = 'Example Directive - Contact Card';
+  /*  Dir1 Ctrl - contact-card */
+  angular.module('templates')
+    .controller('ContactCardCtrl', function($scope) {
+      $scope.pageTitle = 'Example Directive - Contact Card';
 
- $scope.friends = [
-   {
-     id: 0,
-     firstName: 'Will',
-     lastName: 'Smith',
-     age: '40',
-     tweets: [
-       { id: 0, text: 'Wow, that\'s decent uncle Phil!' },
-       { id: 1, text: 'Aunt Viv just had another baby!'},
-       { id: 2, text: 'I love Philly, but nothin\' beats the sunshine of LA'}
+     $scope.friends = [
+       {
+         id: 0,
+         firstName: 'Will',
+         lastName: 'Smith',
+         age: '40',
+         tweets: [
+           { id: 0, text: 'Wow, that\'s decent uncle Phil!' },
+           { id: 1, text: 'Aunt Viv just had another baby!'},
+           { id: 2, text: 'I love Philly, but nothin\' beats the sunshine of LA'}
+         ]
+       },
+       {
+         id: 0,
+         firstName: 'Carlton',
+         lastName: 'Banks',
+         age: '39',
+         tweets: [
+           { id: 0, text: 'I just scored tickets to the Michael Buble concert!' },
+           { id: 1, text: 'Someday I\'ll be super rich. Oh wait I am super rich!'},
+           { id: 2, text: 'I way could live in the ghetto. Could I have a butler?'}
+         ]
+       },
+
      ]
-   },
-   {
-     id: 0,
-     firstName: 'Carlton',
-     lastName: 'Banks',
-     age: '39',
-     tweets: [
-       { id: 0, text: 'I just scored tickets to the Michael Buble concert!' },
-       { id: 1, text: 'Someday I\'ll be super rich. Oh wait I am super rich!'},
-       { id: 2, text: 'I way could live in the ghetto. Could I have a butler?'}
-     ]
-   },
+   });
 
- ]
-})
+ })();
